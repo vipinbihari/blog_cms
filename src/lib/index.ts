6680a7053e8f42596c8a25e87fa5d3924103fa4d
Vipin Bihari <vipinbiharitiwari25@gmail.com>
2025-06-15 21:28:55 +0530

    Restarting This branch from Here

---
+++ src/lib/index.ts
@@ -0,0 +1,32 @@
+// Content utilities
+export * from './content/queries';
+export { getSuggestedPosts, getPostsFromSameCategory, getPostsWithSimilarTags } from './content/related';
+export * from './content/search';
+
+// SEO utilities
+export * from './seo/utils';
+
+// Image utilities
+export * from './images/utils';
+
+// Pagination utilities
+export * from './pagination/utils';
+
+// Re-export commonly used types
+export type {
+  BlogPost,
+  BlogPostData,
+  PostsByYear,
+  CategoryStats,
+  TagStats,
+  SearchResult,
+  PaginationResult,
+  BaseLayoutProps,
+  PostLayoutProps,
+  Theme,
+  Author,
+} from '../types/content';
+
+// Re-export blog configuration
+export { BLOG_CONFIG, getCurrentConfig, getThemeColors } from '../config/current-config';
+export type { BlogConfig } from '../config/blog-template'; 
